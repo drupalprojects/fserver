529f1d0065522417768012aed0a24688f5dd8a50
Randall Knutson <lladnar1@gmail.com>
2011-08-29 20:32:34 -0500

    Initial port to D7.

--- fserver.js
+++ fserver.js
@@ -1,21 +1,27 @@
 // $Id$
 
+(function ($) {
+
 /**
  * JS behaviors for fserver.
  */
-Drupal.behaviors.fserver = function() {
-  $('#edit-field-fserver-method-value:not(.processed)').each(function() {
-    $(this).addClass('processed');
-    $(this).change(function() {
-      switch ($(this).val()) {
-        case '0':
-          $('#edit-field-fserver-repository-0-value-wrapper').hide();
-          break;
-        default:
-          $('#edit-field-fserver-repository-0-value-wrapper').show();
-          break;
-      }
-    });
-    $(this).change();
-  });
-};
+  Drupal.behaviors.fserver = {
+    attach: function (context) {
+      $('#edit-field-fserver-method-und:not(.processed)').each(function() {
+        $(this).addClass('processed');
+        $(this).change(function() {
+          switch ($(this).val()) {
+            case '0':
+              $('#edit-field-fserver-repository').hide();
+              break;
+            default:
+              $('#edit-field-fserver-repository').show();
+              break;
+          }
+        });
+        $(this).change();
+      });
+    }
+  }
+
+})(jQuery);
